0668a9032909a85c580ab04a9b2a2714ba4cd16a
heitor.developer@outlook.com <heitor.souza@kimmais.com.br>
2023-04-03 22:34:54 -0300

    Create Structure Form

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,6 +1,13 @@
 function Formulario(){
     return(
-        <h1>Formulário</h1>
+        <form>
+            <input type='text' placeholder='Nome'/>
+            <input type='text' placeholder='Marca'/>
+            <input type='button' placeholder='Cadastrar'/>
+            <input type='button' placeholder='Alterar'/>
+            <input type='button' placeholder='Remover'/>
+            <input type='button' placeholder='Cancelar'/>
+        </form>
     )
 }
 export default Formulario;
